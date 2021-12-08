6e85efab4fe19661985f5d6e02108bb3c848ae66
Austin  Marrazza <austinmarrazza@Austins-MacBook-Pro.local>
2021-12-07 16:34:52 -0800

    complete

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -8,6 +8,14 @@ const Posts = (props) => {
 
   return (
     <div className='posts-container-wrapper'>
+      {
+        posts.map(post => {
+          const numberOfLikes = post.likes;
+          const comments = post.comments;
+          console.log(numberOfLikes);
+          return <Post post={post} likePost={likePost} numberOfLikes={numberOfLikes} comments={comments}/>
+        })
+      }
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
